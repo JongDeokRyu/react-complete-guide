652abe24d748f6e49545ef463625a10cd6cf40b7
JongDeokRyu <deok8851@gmail.com>
2023-04-25 21:09:32 +0900

    강의 복습

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,6 +7,7 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   const changeTitleHandler = () => {
+    console.log("*** title *** ---> ", props.title);
     setTitle("Update Title");
   };
 
@@ -15,7 +16,7 @@ const ExpenseItem = (props) => {
       <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
         <button onClick={changeTitleHandler}>change title</button>
